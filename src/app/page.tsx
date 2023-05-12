180d0ec0369f881002dc514a152e2e9915d03d90
shell <shell@toss.im>
2023-05-12 19:26:18 +0900

    feat

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,25 +1,22 @@
 import { A } from '@/components/A';
-import { B } from '@/components/B';
-import { C } from '@/components/C';
-import { D } from '@/components/D';
+// import { B } from '@/components/B';
+// import { C } from '@/components/C';
+// import { D } from '@/components/D';
 
 export default function Home() {
-  const f =
-    typeof window === undefined
-      ? undefined
-      : new URL(window.location.href).searchParams.get('f');
+  const f = '';
 
-  if (f === 'b') {
-    return <B />;
-  }
+  // if (f === 'b') {
+  //   return <B />;
+  // }
 
-  if (f === 'c') {
-    return <C />;
-  }
+  // if (f === 'c') {
+  //   return <C />;
+  // }
 
-  if (f === 'd') {
-    return <D />;
-  }
+  // if (f === 'd') {
+  //   return <D />;
+  // }
 
   return <A />;
 }
