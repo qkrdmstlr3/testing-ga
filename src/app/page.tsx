e571e1bab7a733bd3b4cb257eab9386b6ea1bc8e
shell <shell@toss.im>
2023-05-12 19:27:31 +0900

    feat

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,7 +1,15 @@
-import { A } from '@/components/A';
+// import { A } from '@/components/A';
 // import { B } from '@/components/B';
 // import { C } from '@/components/C';
 // import { D } from '@/components/D';
+
+import { useRouter } from 'next/router';
+
+export function A() {
+  const router = useRouter();
+
+  return <button onClick={() => router.push('?f=b')}>A</button>;
+}
 
 export default function Home() {
   const f = '';
